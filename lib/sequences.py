55f0735d961e6729c79fb5ec45b86430ce60e073
Rhiannon Moore <rhiannonmoore10@yahoo.com>
2023-07-03 17:59:41 -0700

    completes lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
+
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+    if length < 1:
+        print(f"[]")
+    elif length < 2:
+        print([fibonacci_sequence[0]], end="\n")
+    elif length < 3:
+        print(fibonacci_sequence[0:2], end="\n")
+    else:
+        print(fibonacci_sequence)
+
+
